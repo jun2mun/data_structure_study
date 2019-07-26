d97bef30cfd65a25a4ae3419e2af41776f79102d
Jun <jun2mun@naver.com>
2019-07-26 15:00:45 +0900

    0723~0726

---
+++ 0726/DBLinkedList.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DBLinkedList.h"
+
+void ListInit(List* plist) {
+	plist->head = NULL;
+	plist->numOfData = 0;
+}
+void LInsert(List* plist, Data data) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head;
+	if (plist->head != NULL)
+		plist->head->prev = newNode;
+
+	newNode->prev = NULL;
+	plist->head = newNode;
+}
+
+int LFirst(List* plist, Data* pdata) {
+	if (plist->head == NULL)
+		return FALSE;
+
+	plist->cur = plist->head;
+	*pdata = plist->cur->data;
+
+	return TRUE;
+}
+
+int LNext(List* plist, Data* pdata) {
+	if (plist->cur->next == NULL)
+		return FALSE;
+
+	plist->cur = plist->cur->next;
+	*pdata = plist->cur->data;
+
+	return TRUE;
+}
+
+int LPrevious(List* plist, Data* pdata) {
+	if (plist->cur->prev == NULL)
+		return FALSE;
+
+	plist->cur = plist->cur->prev;
+	*pdata = plist->cur->data;
+
+	return TRUE;
+}
+
+int LCount(List* plist) {
+	return plist->numOfData;
+}
+int LRemove(List* plist) {
+	Node* rpos = plist->cur;
+	Data rdata = rpos->data;
+
+	if (rpos == plist->head) {
+		plist->head->next = plist->head;
+		plist->head->prev = NULL;
+		plist->cur = NULL;
+	}
+	else {
+		plist->cur->prev->next = plist->cur->next;
+		plist->cur->prev = plist->cur;
+
+	}
+	plist->numOfData--;
+	free(rpos);
+
+	return rdata;
+
+
+}
+//DBLinkedList.c
